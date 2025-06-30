a433b1f75f1d942780cc568424fb28c928b67d17
niamorg <romaing.bis@gmail.com>
2025-06-30 12:00:37 +0200

    changed log directory

--- tdmpc2/common/parser.py
+++ tdmpc2/common/parser.py
@@ -2,6 +2,7 @@ import dataclasses
 import re
 from pathlib import Path
 from typing import Any
+import datetime as dt
 
 import hydra
 from omegaconf import OmegaConf
@@ -54,7 +55,9 @@ def parse_cfg(cfg: OmegaConf) -> OmegaConf:
 			pass
 
 	# Convenience
-	cfg.work_dir = Path(hydra.utils.get_original_cwd()) / 'logs' / cfg.task / str(cfg.seed) / cfg.exp_name
+	# cfg.work_dir = Path(hydra.utils.get_original_cwd()) / 'logs' / cfg.task / str(cfg.seed) / cfg.exp_name
+	now = dt.datetime.now()
+	cfg.work_dir = Path(hydra.utils.get_original_cwd()) / 'outputs' / 'train' / f"{now:%Y-%m-%d}" / cfg.exp_name
 	cfg.task_title = cfg.task.replace("-", " ").title()
 	cfg.bin_size = (cfg.vmax - cfg.vmin) / (cfg.num_bins-1) # Bin size for discrete regression
 
