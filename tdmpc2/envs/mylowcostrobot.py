54551f547e8ae3acf398416a06107ce606e64c27
niamorg <romaing.bis@gmail.com>
2025-07-01 14:03:43 +0200

    mylowcost env maker

--- tdmpc2/envs/mylowcostrobot.py
+++ tdmpc2/envs/mylowcostrobot.py
@@ -1,26 +1,27 @@
 import gymnasium as gym
 from gymnasium.wrappers import FilterObservation, FlattenObservation
-from envs.lowcostrobot import LowCostRobotWrapper
 
-LOWCOSTROBOT_TASKS = {
-    'MyPushCube-v0': 'MyPushCube-v0',
-    'MyReachCube-v0': 'MyReachCube-v0',
-}
+LOWCOSTROBOT_TASKS = ['MyPushCube-v0', 'MyReachCube-v0']
+
+class TerminatedTruncatedToDone(gym.Wrapper):
+    def reset(self):
+        return self.env.reset()[0]
+
+    def step(self, action):
+        obs, reward, terminated, truncated, info = self.env.step(action)
+        info['terminated'], info['truncated'] = terminated, truncated
+        return obs, reward, terminated or truncated, info
+
 
 def make_env(cfg):
     if not cfg.task in LOWCOSTROBOT_TASKS:
         raise ValueError('Unknown task:', cfg.task)
 
-    env = gym.make(
-        f"gym_lowcostrobot:{LOWCOSTROBOT_TASKS[cfg.task]}",
-        max_episode_steps=cfg.env["max_episode_steps"],
-        observation_cameras=cfg.env["observation_cameras"],
-        render_mode='rgb_array'
-        )
+    env = gym.make(f"gym_lowcostrobot:{cfg.task}", **cfg.env["kwargs"], render_mode='rgb_array', disable_env_checker=True)
     
-    env = FilterObservation(env, cfg.env["filter_keys"] + [f'image_{cam}' for cam in env.cameras])
+    env = FilterObservation(env, cfg.env["filter_keys"] + [f'image_{cam}' for cam in env.unwrapped.cameras])
     env = FlattenObservation(env)
-    env = LowCostRobotWrapper(env)
+    env = TerminatedTruncatedToDone(env)
     
     env.max_episode_steps = env.spec.max_episode_steps
     # cfg.discount_max = 0.97 #0.99=tdmpc; 0.9=lerobot
