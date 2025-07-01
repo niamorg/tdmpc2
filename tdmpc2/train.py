bdf65447f692d942c736d318f16e33079f55f873
niamorg <romaing.bis@gmail.com>
2025-07-01 16:48:08 +0200

    torch logs flags

--- tdmpc2/train.py
+++ tdmpc2/train.py
@@ -2,7 +2,7 @@ import os
 os.environ['MUJOCO_GL'] = os.getenv("MUJOCO_GL", 'egl')
 os.environ['LAZY_LEGACY_OP'] = '0'
 os.environ['TORCHDYNAMO_INLINE_INBUILT_NN_MODULES'] = "1"
-os.environ['TORCH_LOGS'] = "+recompiles"
+os.environ['TORCH_LOGS'] = os.getenv("TORCH_LOGS", "+recompiles")
 import warnings
 warnings.filterwarnings('ignore')
 import torch
